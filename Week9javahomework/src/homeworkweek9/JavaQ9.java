a96accd46352bdab816f224cd12c32fd5847c78b
Indiawhite <pdeval2706@gmail.com>
2024-05-01 23:47:22 +0100

    My 1st push to repo

---
+++ Week9javahomework/src/homeworkweek9/JavaQ9.java
@@ -0,0 +1,21 @@
+package homeworkweek9;
+
+import java.util.ArrayList;
+
+public class JavaQ9 {
+    public static void main(String[] args) {
+        ArrayList<String> groupName = new ArrayList<>();
+
+        groupName.add("Java Class 1");
+        groupName.add("Java Class 2");
+        groupName.add("Java Class 3");
+        groupName.add("Java Class 4");
+
+        System.out.println("Group Name :");
+
+        for (String groupNames : groupName);
+        System.out.println(groupName);
+
+        }
+    }
+
